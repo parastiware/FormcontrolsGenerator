d59075136e72834887e9f052e24431312a1af454
Paras Tiwari <parastiware@gmail.com>
2021-07-20 13:16:37 +0545

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace formcontrols
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.Tool = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OptionsCollection = new System.Windows.Forms.ComboBox();
+            this.ButtonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonCreate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonCreate.Location = new System.Drawing.Point(12, 126);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(80, 34);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = false;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(12, 92);
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown.TabIndex = 2;
+            this.numericUpDown.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Tool
+            // 
+            this.Tool.AutoSize = true;
+            this.Tool.Location = new System.Drawing.Point(12, 26);
+            this.Tool.Name = "Tool";
+            this.Tool.Size = new System.Drawing.Size(0, 13);
+            this.Tool.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantity:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tool:";
+            // 
+            // OptionsCollection
+            // 
+            this.OptionsCollection.FormattingEnabled = true;
+            this.OptionsCollection.Location = new System.Drawing.Point(12, 42);
+            this.OptionsCollection.Name = "OptionsCollection";
+            this.OptionsCollection.Size = new System.Drawing.Size(110, 21);
+            this.OptionsCollection.TabIndex = 4;
+            // 
+            // ButtonClear
+            // 
+            this.ButtonClear.BackColor = System.Drawing.Color.Red;
+            this.ButtonClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ButtonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonClear.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ButtonClear.Location = new System.Drawing.Point(12, 166);
+            this.ButtonClear.Name = "ButtonClear";
+            this.ButtonClear.Size = new System.Drawing.Size(107, 34);
+            this.ButtonClear.TabIndex = 0;
+            this.ButtonClear.Text = "Clear";
+            this.ButtonClear.UseVisualStyleBackColor = false;
+            this.ButtonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OptionsCollection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Tool);
+            this.Controls.Add(this.numericUpDown);
+            this.Controls.Add(this.ButtonClear);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Label Tool;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox OptionsCollection;
+        private System.Windows.Forms.Button ButtonClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formcontrols
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        Random X = new Random();
+       public enum ToolOptions
+        {
+            TextBox,Button,Label,CheckBox,RadioButton
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            setValue();
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        void setValue()
+        {
+           
+            foreach(var option in Enum.GetValues(typeof(ToolOptions)))
+            OptionsCollection.Items.Add(option);
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            ToolOptions selectedOption = (ToolOptions)OptionsCollection.SelectedItem;
+            var selectedValue = numericUpDown.Value;
+
+            createTool(selectedOption, selectedValue);
+        }
+
+        private void createTool(ToolOptions selectedOption, decimal selectedValue)
+
+        {
+          
+            
+            switch (selectedOption)
+            {
+
+                case ToolOptions.TextBox: for(int i=0;i<selectedValue;i++)
+                                             {
+                                                    createTextBox();
+                                              }
+                                                break;
+                case ToolOptions.Label: for (int i = 0; i < selectedValue; i++)
+                                        {
+                                                     createLabel();
+                                         }
+                                                break;
+
+                case ToolOptions.Button:
+                    for (int i = 0; i < selectedValue; i++)
+                    {
+                        createButton();
+                    }
+                    break;
+
+                case ToolOptions.CheckBox:
+                    for (int i = 0; i < selectedValue; i++)
+                    {
+                        cretecheckBox();
+                    }
+                    break;
+                case ToolOptions.RadioButton:
+                    for (int i = 0; i < selectedValue; i++)
+                    {
+                        createRadioButton();
+                    }
+                    break;
+
+            }
+
+        }
+        void createTextBox()
+        {
+
+            TextBox textBox = new TextBox();
+            textBox.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+            textBox.Visible = true;
+            textBox.Name = "textbox" + a.ToString();
+            Controls.Add(textBox);
+            a++;
+        }
+        void createLabel()
+        {
+            Label label = new Label();
+            label.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+            label.Text = "Label";
+            label.Visible = true;
+            label.Name = "label" + a.ToString();
+            Controls.Add(label);
+            a++;
+        }
+        void createButton()
+        {
+            Button newButton = new Button();
+            newButton.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+            newButton.Text = "Button";
+            newButton.Visible = true;
+            newButton.Name = "btn" + a.ToString();
+            Controls.Add(newButton);
+            a++;
+        }
+        void cretecheckBox()
+        {
+            CheckBox checkBox = new CheckBox();
+            checkBox.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+            checkBox.Name = "Check box";
+            checkBox.Text = "Check box Option 1";
+            checkBox.Name = "chk" + a.ToString();
+            checkBox.Checked = true;
+            Controls.Add(checkBox);
+            a++;
+
+
+        }
+        void createRadioButton()
+        {
+            RadioButton radioButton = new RadioButton();
+            radioButton.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+            radioButton.Name = "radioButton";
+            radioButton.Text = "radio Option 1";
+            radioButton.Name = "rb" + a.ToString();
+            radioButton.Visible = true;
+            Controls.Add(radioButton);
+            a++;
+
+        }
+        //void createCalender()
+        //{
+        //    MonthCalendar monthCalendar = new MonthCalendar();
+        //    monthCalendar.Location = new Point(X.Next(0, 300), X.Next(0, 300));
+        //    monthCalendar.Visible = true;
+        //    monthCalendar.Name = "mthcldr" + a.ToString();
+        //    Controls.Add(monthCalendar);
+        //    a++;
+        //}
+
+        void clear()
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("You surely want to clear the form", "Alert!!", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                clear();
+            }
+        }
+    }
+}
